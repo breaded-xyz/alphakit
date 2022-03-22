b40daab7615a573108fa8ab723eca29b3842d8f0
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-03-22 16:48:30 +0000

    Outline pseudo code for example and mod init

---
+++ example_test.go
@@ -0,0 +1,18 @@
+package zero2algo
+
+func Example() {
+	// New Backtest Dealer (sensible defaults)
+
+	// New Trendbot (sensible defaults)
+
+	// Load .csv file of historical prices into a dataframe
+
+	// For each row
+	// New price bar from cols
+	// Dealer receive price bar
+	// Bot receive price bar
+
+	// New backtest report from trades and portfoilo curve
+
+	// Print report summary to console
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/colngroup/zero2algo
+
+go 1.18
