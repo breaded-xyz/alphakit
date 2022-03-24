f06781d11824153afb0f89a26efa9424717d3069
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-03-24 20:30:19 +0000

    ✨ feat: concurrent optimize example

--- exampleoptimize_test.go
+++ exampleoptimize_test.go
@@ -3,6 +3,7 @@ package zero2algo
 import (
 	"encoding/csv"
 	"os"
+	"sync"
 
 	"github.com/colngroup/zero2algo/broker/backtest"
 	"github.com/colngroup/zero2algo/optimize"
@@ -32,27 +33,36 @@ func ExampleOptimize() {
 	prices, _ := pricing.NewCSVKlineReader(csv.NewReader(file)).ReadAll()
 
 	// Iterate the test cases, executing each set of params and collecting the results
+	// Test cases are executed concurrently to reduce run time
+	wg := new(sync.WaitGroup)
 	for _, c := range cases {
-		// Create a special simulated dealer for each test case run
-		dealer := backtest.NewDealer()
+		wg.Add(1)
 
-		// Create a new bot initialized with our dealer
-		// HodlBot implements a basic buy and hold algo
-		bot := tradebot.NewHodlBot(dealer)
-		// The bot is configured with the params in the test case
-		_ = bot.Configure(c)
+		go func(c map[string]any) {
+			defer wg.Done()
 
-		// Iterate prices sending each price interval to the dealer and then to the bot
-		for _, price := range prices {
-			_ = dealer.ReceivePrice(price)
-			_ = bot.ReceivePrice(price)
-		}
-		// Close the bot which will liquidate any open position resulting in a final trade
-		bot.Close()
+			// Create a special simulated dealer for each test case run
+			dealer := backtest.NewDealer()
 
-		// Generate a performance report for the test case and add it to the result set
-		results = append(results, perf.NewReport(dealer.ListTrades(), dealer.EquityCurve()))
+			// Create a new bot initialized with our dealer
+			// HodlBot implements a basic buy and hold algo
+			bot := tradebot.NewHodlBot(dealer)
+			// The bot is configured with the params in the test case
+			_ = bot.Configure(c)
+
+			// Iterate prices sending each price interval to the dealer and then to the bot
+			for _, price := range prices {
+				_ = dealer.ReceivePrice(price)
+				_ = bot.ReceivePrice(price)
+			}
+			// Close the bot which will liquidate any open position resulting in a final trade
+			bot.Close()
+
+			// Generate a performance report for the test case and add it to the result set
+			results = append(results, perf.NewReport(dealer.ListTrades(), dealer.EquityCurve()))
+		}(c)
 	}
+	wg.Wait()
 
 	// Rank results based on the test case that produced the highest sharpe ratio
 	optimal := optimize.SharpeSort(results)[0]
