c3e3c0cb93a55f2406de49a1b96cac7192fc3ce9
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-03-31 13:52:08 +0100

    📃 docs: comments

--- broker/backtest/clock.go
+++ broker/backtest/clock.go
@@ -4,35 +4,55 @@ import (
 	"time"
 )
 
+// Compiler hint that Clock must implement Clocker.
 var _ Clocker = (*Clock)(nil)
 
+// Clocker defines a clock to be used by Simulation to timestamp events.
 type Clocker interface {
+
+	// NextEpoch advances the simulation clock to a new time.
 	NextEpoch(time.Time)
+
+	// Now returns a time that is the same as the epoch start time
+	// or incrementally later each time it is called.
 	Now() time.Time
+
+	// Epoch returns the start time of the current epoch.
 	Epoch() time.Time
 }
 
+// Clock is the default Clocker implementation for Simulation.
+// When Now is called an incrementally later time is returned.
+// Each increment is a 'tock' and equals 1 * time.Millisecond.
+// Tock term is used to avoid confusion with 'tick' which has a defined meaning in trading.
+// Clock helps ensure orders are processed in the sequence they are submitted.
 type Clock struct {
 	tock  int64
 	epoch time.Time
 }
 
+// NewClock sets the epoch to the current time.
 func NewClock() Clocker {
 	return &Clock{
 		epoch: time.Now(),
 	}
 }
 
+// NextEpoch advances to the next epoch with the given time.
+// Tock counter is reset. Next time Now is called it will be epoch + 1 tock.
 func (c *Clock) NextEpoch(epoch time.Time) {
 	c.epoch = epoch
 	c.tock = 0
 }
 
+// Now returns the next tock, which is 1 * time.millisecond later than the last call.
+// To reset tock call NextEpoch.
 func (c *Clock) Now() time.Time {
 	c.tock++
 	return c.epoch.Add(time.Duration(c.tock) * time.Millisecond)
 }
 
+// Epoch returns the current epoch start time.
 func (c *Clock) Epoch() time.Time {
 	return c.epoch
 }
--- broker/backtest/stubclock.go
+++ broker/backtest/stubclock.go
@@ -2,20 +2,29 @@ package backtest
 
 import "time"
 
+// Compiler hint that StubClock must implement Clocker.
 var _ Clocker = (*StubClock)(nil)
 
+// StubClock provides a canned time to Simulator for testing.
+// For a discussion of various types of 'test double' see:
+// https://www.martinfowler.com/articles/mocksArentStubs.html
 type StubClock struct {
+
+	// Fixed is the time to return
 	Fixed time.Time
 }
 
+// NextEpoch not implemented.
 func (c *StubClock) NextEpoch(epoch time.Time) {
-	// Fake does nothing
+	// Intentionally empty
 }
 
+// Now returns Fixed.
 func (c *StubClock) Now() time.Time {
 	return c.Fixed
 }
 
+// Epoch returns Fixed.
 func (c *StubClock) Epoch() time.Time {
 	return c.Fixed
 }
