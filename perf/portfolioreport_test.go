c2a29c9b3c5b3aaadb0c3fe1fe059f26cf500b72
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-04-12 21:36:46 +0100

    🧪 test: tradereport test

---
+++ perf/portfolioreport_test.go
@@ -0,0 +1,36 @@
+package perf
+
+import (
+	"testing"
+	"time"
+
+	"github.com/colngroup/zero2algo/broker"
+	"github.com/colngroup/zero2algo/dec"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPortfolioReport(t *testing.T) {
+	datum := time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local)
+	give := broker.EquitySeries{
+		broker.Timestamp(datum.UnixMilli()):                     dec.New(10),
+		broker.Timestamp(datum.Add(24 * time.Hour).UnixMilli()): dec.New(20),
+		broker.Timestamp(datum.Add(96 * time.Hour).UnixMilli()): dec.New(40),
+		broker.Timestamp(datum.Add(48 * time.Hour).UnixMilli()): dec.New(5),
+		broker.Timestamp(datum.Add(72 * time.Hour).UnixMilli()): dec.New(10),
+	}
+	want := PortfolioReport{
+		PeriodStart:  datum,
+		PeriodEnd:    datum.Add(96 * time.Hour),
+		Period:       96 * time.Hour,
+		StartEquity:  10,
+		EndEquity:    40,
+		EquityReturn: 3,
+	}
+	act := NewPortfolioReport(give)
+	assert.Equal(t, want.PeriodStart, act.PeriodStart)
+	assert.Equal(t, want.PeriodEnd, act.PeriodEnd)
+	assert.Equal(t, want.Period, act.Period)
+	assert.Equal(t, want.StartEquity, act.StartEquity)
+	assert.Equal(t, want.EndEquity, act.EndEquity)
+	assert.Equal(t, want.EquityReturn, act.EquityReturn)
+}
---
+++ perf/tradereport_test.go
@@ -0,0 +1,62 @@
+package perf
+
+import (
+	"testing"
+	"time"
+
+	"github.com/colngroup/zero2algo/broker"
+	"github.com/colngroup/zero2algo/dec"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTradeReport(t *testing.T) {
+
+	give := []broker.Trade{
+		{
+			Side:       broker.Buy,
+			Profit:     dec.New(-10),
+			HoldPeriod: time.Hour * 96,
+		},
+		{
+			Side:       broker.Buy,
+			Profit:     dec.New(-20),
+			HoldPeriod: time.Hour * 24,
+		},
+		{
+			Side:       broker.Sell,
+			Profit:     dec.New(100),
+			HoldPeriod: time.Hour * 192,
+		},
+		{
+			Side:       broker.Sell,
+			Profit:     dec.New(10),
+			HoldPeriod: time.Hour * 48,
+		},
+	}
+
+	want := &TradeReport{
+		TradeCount:           4,
+		TotalNetProfit:       80,
+		AvgNetProfit:         20,
+		GrossProfit:          110,
+		GrossLoss:            30,
+		ProfitFactor:         3.6666666666666665,
+		PercentProfitable:    0.5,
+		MaxProfit:            100,
+		MaxLoss:              20,
+		AvgProfit:            55,
+		AvgLoss:              15,
+		MaxLossStreak:        2,
+		Kelly:                0.36363636363636365,
+		OptimalF:             0.37,
+		TotalTimeInMarketSec: 1296000,
+		AvgHoldSec:           324000,
+		winningCount:         2,
+		winningPct:           0.5,
+		losingCount:          2,
+		losingPct:            0.5,
+	}
+
+	act := NewTradeReport(give)
+	assert.Equal(t, want, act)
+}
