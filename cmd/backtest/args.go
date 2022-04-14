962a58147ec992affe3787eeccc6371fd4110255
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-04-14 20:23:21 +0100

    🐳 chore: tidy

--- cmd/backtest/args.go
+++ cmd/backtest/args.go
@@ -12,9 +12,9 @@ var (
 	_outputDir = ".out"
 
 	//_priceDir = "/Users/richklee/Dropbox/dev-share/github.com/thecolngroup/go-alpha/prices/binance/spot/LTCUSDT-H1/Y18192021"
-	_priceDir = "/Users/richklee/Dropbox/dev-share/github.com/thecolngroup/go-alpha/prices/binance/spot/SOLUSDT-H1/2021"
+	//_priceDir = "/Users/richklee/Dropbox/dev-share/github.com/thecolngroup/go-alpha/prices/binance/spot/SOLUSDT-H1/2021"
 	//_priceDir = "/Users/richklee/Dropbox/dev-share/github.com/thecolngroup/go-alpha/prices/binance/spot/BTCUSDT-H1/2021"
-	//_priceDir = "/Users/richklee/Dropbox/dev-share/github.com/thecolngroup/go-alpha/prices/binance/spot/BTCUSDT-H1/Y17181920"
+	_priceDir = "/Users/richklee/Dropbox/dev-share/github.com/thecolngroup/go-alpha/prices/binance/spot/BTCUSDT-H1/Y17181920"
 	//_priceDir = "/Users/richklee/Dropbox/dev-share/github.com/thecolngroup/go-alpha/prices/binance/spot/ETHUSDT-H1/Y18192021"
 
 	_dealerMakeFunc = func() broker.SimulatedDealer {
@@ -50,7 +50,7 @@ var (
 		"sizerScaleF":       {1.0},
 	}*/
 
-	_params = optimize.ParamRange{
+	/*_params = optimize.ParamRange{
 		"inSample":          {false},
 		"asset":             {"solusdt"},
 		"barSize":           {"H1"},
@@ -73,9 +73,9 @@ var (
 		"riskerSDFactor":    {1.5},
 		"sizerF":            {0.25},
 		"sizerScaleF":       {0.5},
-	}
+	}*/
 
-	/*_params = optimize.ParamRange{
+	_params = optimize.ParamRange{
 		"inSample":          {true},
 		"asset":             {"btcusdt"},
 		"barSize":           {"H1"},
@@ -98,5 +98,5 @@ var (
 		"riskerSDFactor":    {1.0},
 		"sizerF":            {0.0},
 		"sizerScaleF":       {1.0},
-	}*/
+	}
 )
