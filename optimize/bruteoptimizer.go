b71e258c84b07854ca6463ecbcadc142f2304dba
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-04-19 20:14:36 +0100

    📃 docs: moving comments

--- optimize/bruteoptimizer.go
+++ optimize/bruteoptimizer.go
@@ -28,6 +28,19 @@ type OptimizerStep struct {
 	Err    error
 }
 
+// BruteOptimizer implements a 'brute-force peak objective' optimization approach which
+// tests all given parameter combinations and selects the highest ranked (peak) param set.
+// Prepare:
+// - Accept 1 or more price data samples (inc. over/under sampling)
+// - Split sample into in-sample (training) and out-of-sample (validation) datasets
+// - Generate 1 or more param sets using the cartesian product of given ranges that define the param space
+// Train:
+// - Execute each algo param set over the in-sample price data
+// - Average the performance for each param set over the in-sample data
+// - Rank the param sets based on the performance objective (Profit Factor, T-Score etc)
+// Validate:
+// - Execute the highest ranked ("trained") algo param over the out-of-sample price data
+// - Accept or reject the hypothesis based on statistical significance of the performance report
 type BruteOptimizer struct {
 	SampleSplitPct float64
 	WarmupBarCount int
