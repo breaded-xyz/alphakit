fa171474ea4736d65f6f9fd1fdfc3e52e07194cd
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-04-27 20:52:59 +0100

    🦄 refactor: var names

--- ta/peak.go
+++ ta/peak.go
@@ -2,14 +2,14 @@ package ta
 
 func Peak(series []float64) bool {
 	t2, t1, t0 := Lookback(series, 2), Lookback(series, 1), Lookback(series, 0)
-	slo1 := Slope(t2, t1)
-	slo2 := Slope(t1, t0)
-	return (slo1 == 1 || slo1 == 0) && slo2 == -1
+	prev := Slope(t2, t1)
+	curr := Slope(t1, t0)
+	return (prev == 1 || prev == 0) && curr == -1
 }
 
 func Valley(series []float64) bool {
 	t2, t1, t0 := Lookback(series, 2), Lookback(series, 1), Lookback(series, 0)
-	slo1 := Slope(t2, t1)
-	slo2 := Slope(t1, t0)
-	return (slo1 == -1 || slo1 == 0) && slo2 == 1
+	prev := Slope(t2, t1)
+	curr := Slope(t1, t0)
+	return (prev == -1 || prev == 0) && curr == 1
 }
