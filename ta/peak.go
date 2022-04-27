923031975c8e898fcfcc27ba92b3db254aeab206
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-04-27 17:45:39 +0100

    🦄 refactor: series inpu

--- ta/peak.go
+++ ta/peak.go
@@ -1,13 +1,15 @@
 package ta
 
-func Peak(t1, t2, t3 float64) bool {
-	slo1 := Slope(t1, t2)
-	slo2 := Slope(t2, t3)
+func Peak(series []float64) bool {
+	t2, t1, t0 := Lookback(series, 2), Lookback(series, 1), Lookback(series, 0)
+	slo1 := Slope(t2, t1)
+	slo2 := Slope(t1, t0)
 	return (slo1 == 1 || slo1 == 0) && slo2 == -1
 }
 
-func Valley(t1, t2, t3 float64) bool {
-	slo1 := Slope(t1, t2)
-	slo2 := Slope(t2, t3)
+func Valley(series []float64) bool {
+	t2, t1, t0 := Lookback(series, 2), Lookback(series, 1), Lookback(series, 0)
+	slo1 := Slope(t2, t1)
+	slo2 := Slope(t1, t0)
 	return (slo1 == -1 || slo1 == 0) && slo2 == 1
 }
