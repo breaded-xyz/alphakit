5e6b9ced18df615e0279fe7f46c0fbb2c89ee6bf
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-05-03 21:14:28 +0100

    refactor: w/ MMI even prediction

--- trader/trend/breakoutpredicter.go
+++ trader/trend/breakoutpredicter.go
@@ -64,13 +64,13 @@ func (p *BreakoutPredicter) Predict() float64 {
 	case ta.CrossUp(p.osc.History(), upper):
 		return score + 0.9
 	case ta.CrossUp(p.osc.History(), 0):
-		return score + 0.6
+		return score + 0.7
 	case ta.CrossUp(p.osc.History(), lower):
-		return score + 0.2
+		return score + 0.3
 	case ta.CrossDown(p.osc.History(), upper):
-		return -(score + 0.2)
+		return -(score + 0.3)
 	case ta.CrossDown(p.osc.History(), 0):
-		return -(score + 0.6)
+		return -(score + 0.7)
 	case ta.CrossDown(p.osc.History(), lower):
 		return -(score + 0.9)
 	}
