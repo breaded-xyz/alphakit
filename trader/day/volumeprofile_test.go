71a2ce476d16df21e380caf7597c0ab4ddc8dc82
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-05-11 08:51:14 +0100

    wip vp

--- trader/day/volumeprofile_test.go
+++ trader/day/volumeprofile_test.go
@@ -11,13 +11,14 @@ import (
 	"github.com/colngroup/zero2algo/ta"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/slices"
 )
 
 const testdataPath string = "../../internal/testdata/"
 
 func TestNewMarketProfile(t *testing.T) {
 
-	givePrices := []float64{10.1, 10.3, 11, 12.1, 3.2, 15}
+	/*givePrices := []float64{10.1, 10.3, 11, 12.1, 3.2, 15}
 	giveVolumes := []float64{10, 8, 22, 19, 20, 5}
 	giveBins := 10
 
@@ -27,27 +28,33 @@ func TestNewMarketProfile(t *testing.T) {
 
 	assert.Equal(t, wantHist, act.Hist)
 
-	spew.Dump(act)
+	spew.Dump(act)*/
 }
 
 func TestMarketProfileWithPriceFile(t *testing.T) {
 
-	file, _ := os.Open(path.Join(testdataPath, "btcusdt-1m-2022-05-06.csv"))
+	file, _ := os.Open(path.Join(testdataPath, "btcusdt-1m-2022-05-05.csv"))
 	defer func() {
 		assert.NoError(t, file.Close())
 	}()
 
 	prices, err := market.NewCSVKlineReader(csv.NewReader(file)).ReadAll()
 	assert.NoError(t, err)
-	priceLevels := make([]float64, len(prices))
-	vols := make([]float64, len(prices))
+	var levels []Level
 	for i := range prices {
-		priceLevels[i] = util.RoundTo(ta.OHLC4(prices[i]), 0.1)
-		vols[i] = util.RoundTo(prices[i].Volume, 1.0)
+		levels = append(levels, Level{
+			Price:  util.RoundTo(ta.OHLC4(prices[i]), 0.1),
+			Volume: util.RoundTo(prices[i].Volume, 1.0),
+		})
 	}
+	slices.SortStableFunc(levels, func(i, j Level) bool {
+		return i.Price < j.Price
+	})
 
 	spew.Dump(prices[0].Start, prices[len(prices)-1].Start)
-	mp := NewVolumeProfile(1440, priceLevels, vols)
+	vp := NewVolumeProfile(1000, levels)
 
-	spew.Dump(mp.POC, mp.VAH, mp.VAL)
+	//spew.Dump(floats.Min(vp.Bins), floats.Min(vp.Hist))
+
+	spew.Dump(vp.VAL, vp.POC, vp.VAH)
 }
