bf93adb9adcda376714f95e153180541678c0c6c
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-05-14 14:19:37 +0100

    wip: npoc study

--- cmd/npocstudy/main.go
+++ cmd/npocstudy/main.go
@@ -21,6 +21,7 @@ type sessionRow struct {
 	YVAH              float64
 	YHigh             float64
 	SessionOpenPrice  float64
+	SessionOpenYVA    bool
 	FirstHourPrice    float64
 	FirstHourVWAP     float64
 	LinRegAlpha       float64
@@ -47,25 +48,25 @@ var (
 
 func main() {
 
-	var err error
-
 	pricePath := "/Users/richklee/Dropbox/dev-share/github.com/colngroup/zero2algo/prices/spot/btcusdt-m1/all"
 
-	prices, err = studyrun.ReadPriceSeries(pricePath)
+	sample, err := studyrun.ReadPriceSeries(pricePath)
 	if err != nil {
 		log.Fatalln(err)
 		os.Exit(1)
 	}
 
-	for i := range prices {
-		fmt.Printf("%s\n", prices[i].Start)
-		_ = receivePrice(prices[i])
+	for i := range sample {
+		fmt.Printf("%s\n", sample[i].Start)
+		_ = receivePrice(sample[i])
 	}
 
 	_ = studyrun.SaveStructToCSV("results.csv", results)
 }
 
 func receivePrice(price market.Kline) error {
+
+	prices = append(prices, price)
 	_ = vwap.Update(price)
 
 	levelsNow := ta.VolumeLevel{
@@ -88,9 +89,13 @@ func receivePrice(price market.Kline) error {
 		})
 
 		vp := ta.NewVolumeProfile(10, ySession)
+
+		openPrice := price.O.InexactFloat64()
+
 		results = append(results, sessionRow{
 			Start:            price.Start.UTC(),
-			SessionOpenPrice: price.O.InexactFloat64(),
+			SessionOpenPrice: openPrice,
+			SessionOpenYVA:   crossed(openPrice, vp.VAL, vp.VAH),
 			YLow:             vp.Low,
 			YVAL:             vp.VAL,
 			YPOC:             vp.POC,
@@ -154,10 +159,10 @@ func receivePrice(price market.Kline) error {
 
 		xs := make([]float64, 60)
 		ys := make([]float64, 60)
-		vwapSeries := ta.Window(vwap.History(), 60)
-		for i := range vwapSeries {
+		firstHourSeries := prices[len(prices)-60:]
+		for i := range firstHourSeries {
 			xs[i] = float64(i)
-			ys[i] = vwapSeries[i]
+			ys[i] = ta.HLC3(firstHourSeries[i])
 		}
 		alpha, beta := stat.LinearRegression(xs, ys, nil, false)
 		r2 := stat.RSquared(xs, ys, nil, alpha, beta)
