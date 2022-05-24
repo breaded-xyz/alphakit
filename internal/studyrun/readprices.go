a8f9dd6870d16a6e05926c17eb049dc86a9c1849
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-05-24 09:22:14 +0100

    refactor: moved read klines in dir to market pkg

--- internal/studyrun/readprices.go
+++ internal/studyrun/readprices.go
@@ -1,11 +1,7 @@
 package studyrun
 
 import (
-	"encoding/csv"
 	"errors"
-	"io/fs"
-	"os"
-	"path/filepath"
 
 	"github.com/thecolngroup/alphakit/market"
 )
@@ -21,43 +17,11 @@ func ReadPricesFromConfig(config map[string]any) ([][]market.Kline, error) {
 	var prices [][]market.Kline
 	for _, v := range paths {
 		path := v.(string)
-		series, err := ReadPriceSeries(path)
+		series, err := market.CSVKlineReadAllDir(path)
 		if err != nil {
 			return nil, err
 		}
 		prices = append(prices, series)
 	}
-	return prices, nil
-}
-
-func ReadPriceSeries(path string) ([]market.Kline, error) {
-	var prices []market.Kline
-
-	err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if d.IsDir() {
-			return nil
-		}
-		if filepath.Ext(path) != ".csv" {
-			return nil
-		}
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		klines, err := market.NewCSVKlineReader(csv.NewReader(file)).ReadAll()
-		if err != nil {
-			return err
-		}
-		prices = append(prices, klines...)
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
 	return prices, nil
 }
