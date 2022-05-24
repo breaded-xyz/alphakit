8810508a84f4d8d3880fdbba70cb6e78bcb786a3
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-05-24 11:03:44 +0100

    refactor: ext lib for writing csv

--- go.mod
+++ go.mod
@@ -19,8 +19,8 @@ require (
 )
 
 require (
-	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/gammazero/deque v0.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/gammazero/deque v0.1.1 // indirect
 	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
 	github.com/gonum/internal v0.0.0-20181124074243-f884aa714029 // indirect
 	github.com/gonum/lapack v0.0.0-20181123203213-e4cdc5a0bff9 // indirect
@@ -29,24 +29,24 @@ require (
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
-	github.com/mitchellh/mapstructure v1.4.3 // indirect
-	github.com/pelletier/go-toml v1.9.4 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
-	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/subosito/gotenv v1.3.0 // indirect
 	github.com/thecolngroup/dec v0.0.0-20220523185153-1a64204cf578
-	github.com/thecolngroup/util v0.0.0-20220523184422-c073590ffa25
-	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	github.com/thecolngroup/util v0.0.2
+	golang.org/x/crypto v0.0.0-20220518034528-6f7dac969898 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/term v0.0.0-20220411215600-e5f449aeb171 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
--- internal/studyrun/writereports.go
+++ internal/studyrun/writereports.go
@@ -1,14 +1,12 @@
 package studyrun
 
 import (
-	"encoding/csv"
 	"fmt"
-	"os"
 	"path/filepath"
 	"time"
 
-	"github.com/jszwec/csvutil"
 	"github.com/thecolngroup/alphakit/optimize"
+	"github.com/thecolngroup/util"
 )
 
 const _filenameFriendlyTimeFormat = "20060102T150405"
@@ -31,7 +29,7 @@ func WriteStudy(path string, study optimize.Study) error {
 func WriteSummaryReports(path string, reports []SummaryReport) error {
 	prefix := time.Now().UTC().Format(_filenameFriendlyTimeFormat)
 	out := filepath.Join(path, fmt.Sprintf("%s-summaryreports.csv", prefix))
-	if err := SaveStructToCSV(out, reports); err != nil {
+	if err := SaveDataToCSV(out, reports); err != nil {
 		return err
 	}
 
@@ -41,55 +39,22 @@ func WriteSummaryReports(path string, reports []SummaryReport) error {
 func WriteBacktestReports(path string, reports []BacktestReport) error {
 	prefix := time.Now().UTC().Format(_filenameFriendlyTimeFormat)
 	out := filepath.Join(path, fmt.Sprintf("%s-backtestreports.csv", prefix))
-	if err := SaveStructToCSV(out, reports); err != nil {
+	if err := SaveDataToCSV(out, reports); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func SaveStructToCSV(filename string, data interface{}) error {
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
-	if err != nil {
-		return err
-	}
-	// nolint:errcheck // Explicit err check via sync at end of function
-	f.Close()
+func SaveDataToCSV(filename string, data interface{}) error {
 
-	// Wrap file in CSV struct encoder
-	w := csv.NewWriter(f)
-	enc := csvutil.NewEncoder(w)
-	enc.Tag = "csv"
-	enc.Register(func(m map[string]any) ([]byte, error) {
+	encMap := func(m map[string]any) ([]byte, error) {
 		return []byte(fmt.Sprint(m)), nil
-	})
-	enc.Register(func(m optimize.ParamMap) ([]byte, error) {
+	}
+
+	encParamMap := func(m optimize.ParamMap) ([]byte, error) {
 		return []byte(fmt.Sprint(m)), nil
-	})
-
-	// Only write header if new file
-	info, err := f.Stat()
-	if err != nil {
-		return err
-	}
-	if info.Size() > 0 {
-		enc.AutoHeader = false
 	}
 
-	// Write report
-	err = enc.Encode(data)
-	if err != nil {
-		return err
-	}
-
-	// Flush and check for errors
-	w.Flush()
-	if w.Error() != nil {
-		return w.Error()
-	}
-	if err := f.Sync(); err != nil {
-		return err
-	}
-
-	return nil
+	return util.WriteToCSV(filename, data, encMap, encParamMap)
 }
