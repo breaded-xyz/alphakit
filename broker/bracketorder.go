06f19d53a9d8f998390704045e6749766a49fdef
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-05-24 12:27:04 +0100

    docs: godocs

--- broker/bracketorder.go
+++ broker/bracketorder.go
@@ -1,5 +1,6 @@
 package broker
 
+// BracketOrder groups together a set of dependent orders to open and manage a new position.
 type BracketOrder struct {
 	Enter Order
 	Stop  Order
--- broker/equityseries.go
+++ broker/equityseries.go
@@ -4,12 +4,15 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+// EquitySeries is a time series of equity values.
 type EquitySeries TimeSeries[decimal.Decimal]
 
+// SortKeys returns a sorted slice of keys in ascending chronological order.
 func (es EquitySeries) SortKeys() []Timestamp {
 	return TimeSeries[decimal.Decimal](es).SortKeys()
 }
 
+// SortValuesByTime returns a sorted slice of values in ascending chronological order.
 func (es EquitySeries) SortValuesByTime() []decimal.Decimal {
 	return TimeSeries[decimal.Decimal](es).SortValuesByTime()
 }
