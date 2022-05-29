fc58e4928365663647f418b78d232335e328328c
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-05-29 19:49:20 +0100

    docs: godoc comments

--- cmd/studyrun/runapp.go
+++ cmd/studyrun/runapp.go
@@ -7,6 +7,7 @@ import (
 	"github.com/thecolngroup/alphakit/trader/trend"
 )
 
+// BuildVersion describes key info to identify the build of the app.
 type BuildVersion struct {
 	GitTag    string
 	GitCommit string
@@ -14,6 +15,10 @@ type BuildVersion struct {
 	User      string
 }
 
+// RunApp is the entrypoint for executing the studyrun cmd outside of the alphakit project.
+// Param args are the cmd line args and excludes the app name.
+// Param botRegistry enables injection of bots to be loaded by string name from config.
+// Param build is optional and will render key build version info to the console during execution.
 func RunApp(args []string, botRegistry map[string]trader.MakeFromConfig, build BuildVersion) error {
 	app := studyrun.App{
 		Args:      args,
