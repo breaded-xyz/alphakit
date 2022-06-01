bdb9b0f0f65f08382223fc345a62cef74cf1df33
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-06-01 19:26:54 +0100

    feat: decoder extension for reading alt price files

--- market/csvklinereader.go
+++ market/csvklinereader.go
@@ -2,39 +2,29 @@ package market
 
 import (
 	"encoding/csv"
-	"errors"
 	"io"
-	"strconv"
-	"time"
-
-	"github.com/shopspring/decimal"
-)
-
-var (
-	// ErrNotEnoughColumns is returned when the CSV price record does not have enough columns.
-	ErrNotEnoughColumns = errors.New("require 5 cols to parse kline")
-
-	// ErrInvalidTimeFormat is returned when the CSV price record does not have a valid time unix milli format.
-	ErrInvalidTimeFormat = errors.New("require col[0] start time in unix millisecond format")
-
-	// ErrInvalidPriceFormat is returned when the CSV price record does not prices in expected format.
-	ErrInvalidPriceFormat = errors.New("require col[1..4] OHLC prices in valid decimal format")
-
-	// ErrInvalidVolumeFormat is returned when the CSV price record does not have a valid volume format.
-	ErrInvalidVolumeFormat = errors.New("require col[5] volume in valid float format")
 )
 
 var _ KlineReader = (*CSVKlineReader)(nil)
 
 // CSVKlineReader is a KlineReader that reads from a CSV file.
 type CSVKlineReader struct {
-	csv *csv.Reader
+	csv     *csv.Reader
+	decoder CSVKlineDecoder
 }
 
 // NewCSVKlineReader creates a new CSVKlineReader.
 func NewCSVKlineReader(csv *csv.Reader) *CSVKlineReader {
 	return &CSVKlineReader{
-		csv: csv,
+		csv:     csv,
+		decoder: BinanceCSVKlineDecoder,
+	}
+}
+
+func NewCSVKlineReaderWithDecoder(csv *csv.Reader, decoder CSVKlineDecoder) *CSVKlineReader {
+	return &CSVKlineReader{
+		csv:     csv,
+		decoder: decoder,
 	}
 }
 
@@ -47,7 +37,7 @@ func (r *CSVKlineReader) Read() (Kline, error) {
 		return k, err
 	}
 
-	return newKlineFromCSVRecord(rec)
+	return r.decoder(rec)
 }
 
 // ReadAll reads all the Klines from the underlying CSV data.
@@ -65,40 +55,4 @@ func (r *CSVKlineReader) ReadAll() ([]Kline, error) {
 	}
 
 	return ks, nil
-}
-
-func newKlineFromCSVRecord(record []string) (Kline, error) {
-	var k, empty Kline
-	var err error
-
-	if len(record) < 5 {
-		return k, ErrNotEnoughColumns
-	}
-
-	msec, err := strconv.ParseInt(record[0], 10, 64)
-	if err != nil {
-		return empty, ErrInvalidTimeFormat
-	}
-	k.Start = time.UnixMilli(msec).UTC()
-
-	if k.O, err = decimal.NewFromString(record[1]); err != nil {
-		return empty, ErrInvalidPriceFormat
-	}
-	if k.H, err = decimal.NewFromString(record[2]); err != nil {
-		return empty, ErrInvalidPriceFormat
-	}
-	if k.L, err = decimal.NewFromString(record[3]); err != nil {
-		return empty, ErrInvalidPriceFormat
-	}
-	if k.C, err = decimal.NewFromString(record[4]); err != nil {
-		return empty, ErrInvalidPriceFormat
-	}
-
-	if len(record) > 5 {
-		if k.Volume, err = strconv.ParseFloat(record[5], 64); err != nil {
-			return empty, ErrInvalidVolumeFormat
-		}
-	}
-
-	return k, nil
 }
--- market/csvklinereader_test.go
+++ market/csvklinereader_test.go
@@ -10,17 +10,16 @@ import (
 	"github.com/thecolngroup/dec"
 )
 
-func TestCSVKlineReader_Read(t *testing.T) {
+var assertKlineEq = func(t *testing.T, exp, act Kline) {
+	assert.Equal(t, exp.Start, act.Start)
+	assert.True(t, exp.O.Equal(act.O))
+	assert.True(t, exp.H.Equal(act.H))
+	assert.True(t, exp.L.Equal(act.L))
+	assert.True(t, exp.C.Equal(act.C))
+	assert.Equal(t, exp.Volume, act.Volume)
+}
 
-	var assertKlineEq = func(t *testing.T, exp, act Kline) {
-		assert.Equal(t, exp.Start, act.Start)
-		assert.True(t, exp.O.Equal(act.O))
-		assert.True(t, exp.H.Equal(act.H))
-		assert.True(t, exp.L.Equal(act.L))
-		assert.True(t, exp.C.Equal(act.C))
-		assert.Equal(t, exp.Volume, act.Volume)
-	}
-
+func TestCSVKlineReader_ReadWithBinanceDecoder(t *testing.T) {
 	tests := []struct {
 		name string
 		give string
@@ -79,7 +78,7 @@ func TestCSVKlineReader_Read(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			reader := NewCSVKlineReader(csv.NewReader(strings.NewReader(tt.give)))
+			reader := NewBinanceCSVKlineReader(csv.NewReader(strings.NewReader(tt.give)))
 			kline, err := reader.Read()
 			assert.Equal(t, tt.err, err)
 			assertKlineEq(t, tt.want, kline)
@@ -87,7 +86,7 @@ func TestCSVKlineReader_Read(t *testing.T) {
 	}
 }
 
-func TestCSVKlineReader_ReadAll(t *testing.T) {
+func TestCSVKlineReader_ReadAllWithDefaultDecoder(t *testing.T) {
 	records := []string{
 		"1609459200000,28923.63000000,29031.34000000,28690.17000000,28995.13000000,2311.81144500",
 		"1609459300000,28928.63000000,30031.34000000,22690.17000000,28495.13000000,3000.00",
@@ -96,4 +95,59 @@ func TestCSVKlineReader_ReadAll(t *testing.T) {
 	klines, err := reader.ReadAll()
 	assert.NoError(t, err)
 	assert.Len(t, klines, 2)
+}
+
+func TestCSVKlineReader_ReadWithMetaTraderDecoder(t *testing.T) {
+
+	tests := []struct {
+		name string
+		give string
+		want Kline
+		err  error
+	}{
+		{
+			name: "Read DOHLCV",
+			give: "11/12/2008;00:00;779.527679;780.964756;777.527679;779.964756;5",
+			want: Kline{
+				Start:  time.Date(2008, 12, 11, 0, 0, 0, 0, time.UTC),
+				O:      dec.New(779.527679),
+				H:      dec.New(780.964756),
+				L:      dec.New(777.527679),
+				C:      dec.New(779.964756),
+				Volume: 5},
+			err: nil,
+		},
+		{
+			name: "Not enough columns",
+			give: "1609459200000;28923.63000000;29031.34000000",
+			want: Kline{},
+			err:  ErrNotEnoughColumns,
+		},
+		{
+			name: "Invalid time format",
+			give: "23/12/2021;t;28923.63000000;29031.34000000;28690.17000000;28995.13000000",
+			want: Kline{},
+			err:  ErrInvalidTimeFormat,
+		},
+		{
+			name: "Invalid price format",
+			give: "11/12/2008;00:00;sixty;29031.34000000;28690.17000000;28995.13000000",
+			want: Kline{},
+			err:  ErrInvalidPriceFormat,
+		},
+		{
+			name: "Invalid volume format",
+			give: "11/12/2008;00:00;779.527679;780.964756;777.527679;779.964756;vol",
+			want: Kline{},
+			err:  ErrInvalidVolumeFormat,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reader := NewMetaTraderCSVKlineReader(csv.NewReader(strings.NewReader(tt.give)))
+			kline, err := reader.Read()
+			assert.Equal(t, tt.err, err)
+			assertKlineEq(t, tt.want, kline)
+		})
+	}
 }
