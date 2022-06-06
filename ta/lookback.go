a6eb3522e8ee421ef6fe6090a256abf6ee16a76a
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-06-06 16:27:43 +0100

    feat: optimized lookback helpers

--- ta/lookback.go
+++ ta/lookback.go
@@ -3,31 +3,35 @@ package ta
 // Lookback returns a value from series at n index ago.
 // Series must be in chronological order, with the earliest value at slice index 0.
 // n = 0 returns the latest value. n = 1 returns the value before the latest etc.
-func Lookback(series []float64, n int) float64 {
+func Lookback[T any](series []T, n int) T {
+	var empty T
 	i := (len(series) - n) - 1
 	if i < 0 {
-		return 0
+		return empty
 	}
 	return series[i]
 }
 
 // Window returns a copied slice of series starting at n index ago.
 // Semantics of n argument are the same as Lookback function.
-func Window(series []float64, n int) []float64 {
-	window := make([]float64, len(series))
-	copy(window, series)
+func Window[T any](series []T, n int) []T {
 
 	ln := len(series)
 	if ln <= n {
+		window := make([]T, ln)
+		copy(window, series)
 		return window
 	}
 
 	i := (ln - n) - 1
-	return window[i:]
+	window := make([]T, n+1)
+	copy(window, series[i:])
+
+	return window
 }
 
 // WindowAppend appends a value to the end of the series and slices it to the window starting at n index ago.
 // Semantics of n argument are the same as Window and Lookback functions.
-func WindowAppend(series []float64, n int, v float64) []float64 {
+func WindowAppend[T any](series []T, n int, v T) []T {
 	return Window(append(series, v), n)
 }
